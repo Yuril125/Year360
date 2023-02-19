b89d970599152c1a903bfb6a58a8f0da958e0f70
Yuril <YurilAvali@protonmail.com>
2023-02-19 12:21:07 -0500

    Fix for when Gregorian year is between 0000 and 0099

--- Gregorian-Year360.js
+++ Gregorian-Year360.js
@@ -6,7 +6,7 @@ const gregorianMonth = Number(prompt("Enter Gregorian month (1-12): "));
 const gregorianDay = Number(prompt("Enter Gregorian day (1-31): "));
 
 const gregorianDate = new Date(gregorianYear, gregorianMonth - 1, gregorianDay);
-gregorianDate.setFullYear(gregorianYear);
+gregorianDate.setFullYear(gregorianYear);   // In case 0 <= gregorianYear <= 99
 
 y360Date = new Year360Date(gregorianDate);
 console.log("\n" + y360Date);
